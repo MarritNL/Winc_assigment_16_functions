93047a5225ac8d64bf492a2fdcee6bddccd24dbd
MarritNL <86615967+MarritNL@users.noreply.github.com>
2021-07-25 14:56:52 +0200

    Add files via upload
    functions

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const makePizza = function () {
+    console.log('make dough');
+    console.log('knead dough');
+    console.log('make tomato sauce');
+    console.log('create pizza bottom');
+    console.log('put sauce on pizza bottom');
+    console.log('put cheese on pizza bottom');
+    console.log('------- take a break-------')
+};
+const makeSushi = function () {
+    console.log('boil rice');
+    console.log('add rice vinegar');
+    console.log('slice salmon');
+    console.log('roll salmon in rice in seaweed');
+    console.log('slice roll');
+    console.log('------- take a break-------')
+
+};
+
+makePizza ();
+makePizza ();
+makeSushi ();
+makeSushi ();
+makeSushi ();
